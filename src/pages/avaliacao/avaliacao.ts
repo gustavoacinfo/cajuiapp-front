73f50e26dfa05cff86c9f55954b66213c4a3a068
unknown <gustavo.acinfo@gmail.com>
2019-10-18 16:59:15 -0300

    Listagem das avaliacoes do aluno

--- src/pages/avaliacao/avaliacao.ts
+++ src/pages/avaliacao/avaliacao.ts
@@ -1,6 +1,8 @@
+import { NotaAvaliacaoService } from './../../services/domain/nota-avaliacao.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProfessorOfertaDTO } from '../../models/professoroferta.dto';
+import { NotaAvaliacaoDTO } from '../../models/nota-avaliacao.dto';
 
 /**
  * Generated class for the AvaliacaoPage page.
@@ -18,14 +20,40 @@ export class AvaliacaoPage {
 
   oferta : ProfessorOfertaDTO;
 
+  items : NotaAvaliacaoDTO[];
+
+  pontosDistribuidos : number;
+
+  pontosObtidos : number;
+
   constructor(
     public navCtrl: NavController, 
-    public navParams: NavParams) {
+    public navParams: NavParams,
+    public notaavaliacaoService : NotaAvaliacaoService) {
 
       this.oferta = navParams.data.obj;
   }
 
   ionViewDidLoad() {
+
+    this.notaavaliacaoService.avaliacoesPorOferta(this.oferta.ofertaId.id)
+    .subscribe(response => {
+      this.items = response;
+    },
+    error => {});
+
+    this.notaavaliacaoService.pontosDistribuidos(this.oferta.ofertaId.id)
+    .subscribe(response => {
+      this.pontosDistribuidos = response;
+    },
+    error => {});
+
+    this.notaavaliacaoService.pontosObtidos(this.oferta.ofertaId.id)
+    .subscribe(response => {
+      this.pontosObtidos = response;
+    },
+    error => {});
+
   }
 
   home(){
