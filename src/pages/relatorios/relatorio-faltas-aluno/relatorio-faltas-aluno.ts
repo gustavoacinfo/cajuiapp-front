b5d976ecffe47db3ade2ddea69d48e0581f1caf6
unknown <gustavo.acinfo@gmail.com>
2019-11-27 15:56:56 -0300

    Implementando relatorio de faltas do aluno

--- src/pages/relatorios/relatorio-faltas-aluno/relatorio-faltas-aluno.ts
+++ src/pages/relatorios/relatorio-faltas-aluno/relatorio-faltas-aluno.ts
@@ -1,3 +1,6 @@
+import { RegistroDTO } from './../../../models/registro.dto';
+import { FaltaService } from './../../../services/domain/falta.service';
+import { RegistroService } from './../../../services/domain/registro.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { LogoutPage } from '../../login/login';
@@ -8,6 +11,7 @@ import { ProfessorOfertaService } from '../../../services/domain/professoroferta
 import { UsuarioDTO } from '../../../models/usuario.dto';
 import { ContratoDTO } from '../../../models/contrato.dto';
 import { ProfessorOfertaDTO } from '../../../models/professoroferta.dto';
+import { FaltaDTO } from '../../../models/falta.dto';
 
 /**
  * Generated class for the RelatorioFaltasAlunoPage page.
@@ -31,6 +35,20 @@ export class RelatorioFaltasAlunoPage {
 
   profOfertaId : ProfessorOfertaDTO;
 
+  registros : RegistroDTO[];
+
+  faltas : FaltaDTO[];
+
+  datasFaltas : Array<FaltaDTO>;
+
+  quantRegistros : number;
+
+  quantFrequencia : number;
+
+  numFaltas : number;
+
+  taxaFrequencia : number;
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
@@ -38,10 +56,15 @@ export class RelatorioFaltasAlunoPage {
     public usuarioService : UsuarioService,
     public storage : StorageService,
     public contratoService : ContratoService,
-    public professorOfertaService : ProfessorOfertaService,) {
+    public professorOfertaService : ProfessorOfertaService,
+    public registroService : RegistroService,
+    public faltaService : FaltaService) {
   }
 
   ngOnInit() {
+
+    this.datasFaltas = new Array();
+
     this.usuarioService.findByUsername(this.storage.getLocalUser().username)
       .subscribe(response => {
         this.usuario = response;
@@ -66,7 +89,27 @@ export class RelatorioFaltasAlunoPage {
 
   relatorioFaltas(){
 
-    console.log(this.profOfertaId);
+    this.faltaService.faltasPorOferta(this.profOfertaId.ofertaId.id, this.usuario.id)
+    .subscribe(response => {
+      this.faltas = response;
+      this.quantFrequencia = this.faltas.length;
+      let falta = 0;
+      for(let i=0; i<this.quantFrequencia; i++){
+        if(this.faltas[i].presenca === false){
+          falta = falta + 1;
+          this.adicionaFaltas(this.faltas[i]);
+        }
+      }
+
+      if(this.quantFrequencia > 0){
+        this.numFaltas = falta;
+        this.taxaFrequencia = (this.numFaltas * 100) / this.quantFrequencia;
+      }else{
+        this.taxaFrequencia = 0;
+      }
+      
+    },
+    error => {});
 
   }
 
@@ -77,6 +120,10 @@ export class RelatorioFaltasAlunoPage {
 
   home(){
     this.navCtrl.push('HomePage');
+  }
+
+  adicionaFaltas(falta : FaltaDTO){
+    this.datasFaltas.push(falta);
   }
 
 
